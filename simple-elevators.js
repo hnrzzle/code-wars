b33b0c838c541e609a0e81d8cc13176016e5fe03
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-17 11:11:47 -0700

    simple elevators

---
+++ simple-elevators.js
@@ -0,0 +1,13 @@
+function goto(level,button){
+    if(typeof button !== "string") return 0;
+    const buttonNum = parseInt(button);
+    
+    if(buttonNum > 3 || buttonNum < 0) return 0;
+    if(level > 3 || level < 0) return 0;
+    if(Number.isInteger(buttonNum) && Number.isInteger(level)) {
+      const levelDiff = buttonNum - level;
+      return levelDiff;
+    } else return 0;
+}
+
+// kata found here http://www.codewars.com/kata/simple-elevator/javascript
